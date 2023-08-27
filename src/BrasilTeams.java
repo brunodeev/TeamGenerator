759331a680f93366e83ecd31c3fbcdf88b51b8f1
Bruno César <bcgmeireles@gmail.com>
2023-08-27 16:18:16 -0300

    feat: add print france teams

---
+++ src/BrasilTeams.java
@@ -0,0 +1,15 @@
+
+public class BrasilTeams {
+    public String[] franceTeams = {
+            "Paris Saint-Germain",
+            "Lyon",
+            "Olympique de Marselha",
+            "Lille",
+            "Monaco",
+            "Reims",
+            "Estrasburgo",
+            "Rennes",
+            "Nice",
+            "Nantes",
+    };
+}
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,28 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Leagues leagues = new Leagues();
+        FranceTeams france = new FranceTeams();
+
         Random rand = new Random();
         int randomNumber = rand.nextInt(leagues.leagues.length);
 
         System.out.println(leagues.leagues[randomNumber]);
+
+        switch (leagues.leagues[randomNumber]){
+            case "Brasileirão" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+            case "Premier League" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+            case "Serie A" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+            case "Ligue 1" : System.out.println(france.franceTeams[2]);
+                break;
+            case "Bundesliga" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+            case "Eredivise" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+            case "Liga Portugal" : System.out.println(leagues.leagues[randomNumber]);
+                break;
+        }
     }
 }
